4fd2522ae9b22bcc96ccd1b203e85597c039003f
Krishna Maniya <krishnamaniya19@gmail.com>
2025-02-12 17:14:59 +0530

    sidebar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./Components/Navbar";
-
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Sidebar />
       <h1 className="mt-20 text-center"></h1>
     </div>
   );
